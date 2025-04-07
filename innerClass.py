4b669266d12dd73907c550f57621a2b8a9e38fe8
Nivas Malligesan <nivasmalligesan23@gmail.com>
2025-04-07 14:25:26 +0530

    Update innerClass.py

--- innerClass.py
+++ innerClass.py
@@ -12,8 +12,12 @@ class student:
             self.cpu = 'i5'
             self.ram = 8
             
+        def show(self):
+            print(self.brand,self.cpu,self.ram)
+            
         
 s1 = student('Nivas',2)
 s2 = student('Kiran',3)
-print(s1.lap.brand) #how to access innerClass
-s1.show()
+lap1 = student.laptop() #can be used like this also
+lap1.show() #this is for class laptop
+s1.show() #this is for student
