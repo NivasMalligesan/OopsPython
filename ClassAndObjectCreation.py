b7ef439de3cc88dfb4850a42bf9f66d3c5f8b926
Nivas Malligesan <nivasmalligesan23@gmail.com>
2025-04-07 11:56:14 +0530

    Create ClassAndObjectCreation.py

---
+++ ClassAndObjectCreation.py
@@ -0,0 +1,7 @@
+class computer:#class
+    def config(self): #method
+        print("i5 16gb")
+
+#object creation
+comp1  = computer() #constructer
+computer.config(comp1) # passing the object in bracket since they have different behaviour they have their own behaviour data 
